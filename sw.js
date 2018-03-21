2b9a9adf85788da565222ed4a56248c9075875c5
Lucien  Dulac <lucien.dulac@ekino.com>
2018-03-21 09:58:25 -0500

    deletion of "console.log" line

--- sw.js
+++ sw.js
@@ -11,7 +11,6 @@ const urlToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache');
       return cache.addAll(urlToCache);
     }, error => console.error('Open cache failed :', error))
   );
