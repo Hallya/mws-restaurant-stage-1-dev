a7ef28c6ecf3067ba9ea0ac9d33a6284fd1ce2a0
Lucien  Dulac <lucien.dulac@ekino.com>
2018-04-12 15:31:24 +0200

    trying to solve the same problem

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 
-const CACHE_STATIC = 'static-cache-9';
+const CACHE_STATIC = 'static-cache-10';
 const URLS_TO_CACHE = [
   'index.html',
   'restaurant.html',
